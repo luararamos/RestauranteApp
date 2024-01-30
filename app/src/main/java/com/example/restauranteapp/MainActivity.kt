a30247895d8a39cd8b8f0055b2e472cc5200574a
Lise <35637366+luararamos@users.noreply.github.com>
2024-01-29 22:18:59 -0300

    Jetpack Compose

--- app/src/main/java/com/example/restauranteapp/MainActivity.kt
+++ app/src/main/java/com/example/restauranteapp/MainActivity.kt
@@ -3,44 +3,82 @@ package com.example.restauranteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.restauranteapp.list_item.FoodItem
+import com.example.restauranteapp.model.Food
 import com.example.restauranteapp.ui.theme.RestauranteAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RestauranteAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            FoodList()
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+private fun FoodList(){
+    val foodList: MutableList<Food> = mutableListOf(
+        Food(
+            imgFood = R.drawable.food1,
+            foodName = "Food 1",
+            foodDescription = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut",
+            price = "$ 120.00"
+        ),
+        Food(
+            imgFood = R.drawable.food2,
+            foodName = "Food 2",
+            foodDescription = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut",
+            price = "$ 90.00"
+        ),
+        Food(
+            imgFood = R.drawable.food3,
+            foodName = "Food 3",
+            foodDescription = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut",
+            price = "$ 75.00"
+        ),
+        Food(
+            imgFood = R.drawable.food4,
+            foodName = "Food 4",
+            foodDescription = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut",
+            price = "$ 65.00"
+        ),
+        Food(
+            imgFood = R.drawable.food5,
+            foodName = "Food 5",
+            foodDescription = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut",
+            price = "$ 95.00"
+        )
     )
-}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        LazyColumn(){
+            itemsIndexed(foodList){ position, food ->
+                FoodItem(food)
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RestauranteAppTheme {
-        Greeting("Android")
+        }
+
     }
+}
+@Composable
+@Preview
+private fun Preview(){
+    FoodList()
 }
